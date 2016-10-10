5a27d110a9ba9eee537c8781caafac14a2fe9a42
sumanmaity112 <suman.maity112@gmail.com>
2016-10-10 19:18:32 +0530

    Suman | Added test

---
+++ test/finiteAutomata/DFATest.java
@@ -0,0 +1,116 @@
+package finiteAutomata;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import tuples.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class DFATest {
+    private Alphabet alphabet0, alphabet1, alphabet2;
+    private State q0, q1, q2;
+    private State initialState;
+    private ArrayList<State> states, finalStates;
+    private HashMap<State, HashMap<Alphabet, State>> transitions;
+    private AlphabetSet alphabetSet;
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Before
+    public void setUp() throws Exception {
+        ArrayList<Alphabet> alphabets = new ArrayList<>();
+        alphabet0 = new Alphabet("0");
+        alphabet1 = new Alphabet("1");
+        alphabet2 = new Alphabet("2");
+        alphabets.add(alphabet0);
+        alphabets.add(alphabet1);
+        alphabets.add(alphabet2);
+        alphabetSet = new AlphabetSet(alphabets);
+        q0 = new State("q0");
+        q1 = new State("q1");
+        q2 = new State("q2");
+
+        initialState = q0;
+
+        states = new ArrayList<>();
+        states.add(q0);
+        states.add(q1);
+        states.add(q2);
+
+        finalStates = new ArrayList<>();
+        finalStates.add(q0);
+
+        transitions = new HashMap<>();
+        HashMap<Alphabet, State> q0Transition = new HashMap<>();
+        HashMap<Alphabet, State> q1Transition = new HashMap<>();
+        HashMap<Alphabet, State> q2Transition = new HashMap<>();
+
+        q0Transition.put(alphabet0, q0);
+        q0Transition.put(alphabet1, q1);
+        q0Transition.put(alphabet2, q2);
+
+        q1Transition.put(alphabet0, q1);
+        q1Transition.put(alphabet1, q2);
+        q1Transition.put(alphabet2, q0);
+
+        q2Transition.put(alphabet0, q2);
+        q2Transition.put(alphabet1, q0);
+        q2Transition.put(alphabet2, q1);
+
+        transitions.put(q0, q0Transition);
+        transitions.put(q1, q1Transition);
+        transitions.put(q2, q2Transition);
+
+    }
+
+    @Test
+    public void testIsRecognizeShouldReturnTrueForARecognizedInput() throws Exception {
+        DFA dfa = new DFA(new States(states), initialState, alphabetSet, new FinalStates(finalStates), transitions);
+        Alphabet inputs[] = {alphabet1, alphabet1, alphabet1, alphabet1, alphabet0, alphabet2};
+        assertTrue(dfa.isRecognize(inputs));
+    }
+
+    @Test
+    public void testIsRecognizeShouldReturnFalseForANotRecognizedInput() throws Exception {
+        DFA dfa = new DFA(new States(states), initialState, alphabetSet, new FinalStates(finalStates), transitions);
+        Alphabet inputs[] = {alphabet1, alphabet1, alphabet1, alphabet1, alphabet0, alphabet2, alphabet2};
+        assertFalse(dfa.isRecognize(inputs));
+    }
+
+    @Test
+    public void testIsRecognizeShouldThrowARuntimeExceptionForInvalidAlphabetInGivenInput() throws Exception {
+        DFA dfa = new DFA(new States(states), initialState, alphabetSet, new FinalStates(finalStates), transitions);
+        Alphabet alphabet3 = new Alphabet("3");
+        Alphabet inputs[] = {alphabet1, alphabet1, alphabet1, alphabet1, alphabet0, alphabet2, alphabet2, alphabet3};
+        thrown.expect(RuntimeException.class);
+        thrown.expectMessage("Please check input 11110223");
+        dfa.isRecognize(inputs);
+    }
+
+    @Test
+    public void testIsRecognizeShouldThrowARuntimeExceptionForInvalidStateInGivenTransitionTable() throws Exception {
+        HashMap<Alphabet, State> q3Transition = new HashMap<>();
+
+        q3Transition.put(alphabet0, q0);
+        q3Transition.put(alphabet1, q1);
+        q3Transition.put(alphabet2, q2);
+        HashMap<Alphabet, State> q1State = transitions.get(q1);
+        q1State.remove(alphabet0, q2);
+        State q3 = new State("q3");
+        q1State.put(alphabet0, q3);
+        transitions.put(q3, q3Transition);
+
+        DFA dfa = new DFA(new States(states), initialState, alphabetSet, new FinalStates(finalStates), transitions);
+        Alphabet inputs[] = {alphabet1, alphabet1, alphabet1, alphabet1, alphabet0, alphabet2, alphabet2};
+        thrown.expect(RuntimeException.class);
+        thrown.expectMessage("Please check your transitions table");
+        dfa.isRecognize(inputs);
+    }
+}
