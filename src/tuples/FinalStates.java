6872a64160e661789bc22ad318fe436a1d0e663f
sumanmaity112 <suman.maity112@gmail.com>
2016-10-12 13:42:55 +0530

    Suman | Merge FinalStates and States using extends

--- src/tuples/FinalStates.java
+++ src/tuples/FinalStates.java
@@ -1,16 +1,9 @@
 package tuples;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 
-public class FinalStates {
-    private HashSet<State> finalStates;
-
+public class FinalStates  extends States{
     public FinalStates(ArrayList<State> states) {
-        this.finalStates = new HashSet<State>(states);
-    }
-
-    public boolean contains(State state) {
-        return finalStates.contains(state);
+        super(states);
     }
 }
